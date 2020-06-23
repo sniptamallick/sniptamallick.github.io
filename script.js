86704c3b4b0721cb5f11e970b1e97ea51585823f
Snipta Mallick <39253021+sniptamallick@users.noreply.github.com>
2020-06-23 14:40:21 -0500

    initial personal website commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var email = document.getElementById("#email");
+
+email.addEventListener('click', function()
+{
+    window.open('mailto:mallicksnip@gmail.com');
+});
+
+var linkedin= document.getElementById("#linkedin");
+
+linkedin.addEventListener('click', function()
+{
+    window.open("https://www.linkedin.com/in/sniptamallick/");
+});
+
+var github= document.getElementById("#github");
+
+github.addEventListener('click', function()
+{
+    window.open("https://github.com/sniptamallick");
+});
